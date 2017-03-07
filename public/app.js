2aea08a2230f1c24c52ce62cd10ed7494e46bc6c
Jesse <woah@yehrye.com>
2017-03-07 00:30:25 -0800

    Bam

--- public/app.js
+++ public/app.js
@@ -3,4 +3,14 @@ $(function() {
         var comicSource = $(this).attr('source');
         window.open('/comics_high/' + comicSource + '.bmp', '_blank');
     });
+
+    $(".comicLink.low").on('click', function() {
+        var comicSource = $(this).attr('source');
+        window.open('/comics_low/' + comicSource + '.JPG', '_blank');
+    });
+
+    $(".comicLink.character").on('click', function() {
+        var comicSource = $(this).attr('source');
+        window.open('/characters/' + comicSource + '.jpg', '_blank');
+    });
 });
--- whew.js
+++ whew.js
@@ -13,22 +13,21 @@ app.set('view engine', 'pug');
 app.use(express.static(web_root));
 app.use(compress());
 
+const getFiles = function(dir) {
+    let files = fs.readdirSync(web_root + '/' + dir);
+
+    return files.map(function(file) {
+        return path.parse(file).name;
+    });
+};
+
 // COw links to /index.php like a total scrub
 app.get(['/', '/index.php'], function(req, res) {
-    fs.readdir(web_root + '/comics_high', function(err, files) {
-        if (err != null) {
-            throw err;
-        }
+    let highComicsWup = getFiles('comics_high').join('\n');
+    let lowComicsWup = getFiles('comics_low').join('\n');
+    let charactersWup = getFiles('characters').join('\n');
 
-        files = files.map(function(file) {
-            return path.parse(file).name;
-        });
-
-        let highComicsWup = files.join('\n');
-        res.render('whew', {highComicsWup: highComicsWup});
-    });
-
-
+    res.render('whew', {highComicsWup: highComicsWup, lowComicsWup: lowComicsWup, charactersWup: charactersWup});
 });
 
 app.listen(HTTP_PORT, function() {
